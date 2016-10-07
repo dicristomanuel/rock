61357d3317a92406655a0977913590a1d589ae41
dicristomanuel <manuel.dicristo@raise.com>
2016-10-07 09:58:50 -0500

    [app] setting up

---
+++ src/components/base-component-react/_package/template.js
@@ -0,0 +1,1 @@
+var oc=oc||{};oc.components=oc.components||{},oc.components["737b5c8ac8536a42972facd7f2bbfc94532react"]={compiler:[7,">= 4.0.0"],main:function(c,o,n,a,e){return"  "},useData:!0};
---
+++ src/components/base-component-react/webpack.config.js
@@ -0,0 +1,26 @@
+// var path = require('path');
+//
+// module.exports = {
+//   entry: './react/index.js',
+//   output: {
+//     path: "./public/assets/js",
+//     filename: "bundle.js",
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         text: /\.jsx?$/,
+//         exclude: /(node_modules)/,
+//         loader: 'babel',
+//         query: {
+//           presets: ['react', 'es2015'],
+//         }
+//       }
+//     ]
+//   },
+//   resolve: {
+//     root: path.resolve('./app'),
+//     extensions: ['', '.js', '.jsx'],
+//   }
+// };
+// check how HTML is being exported
